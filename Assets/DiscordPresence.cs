909201411226592409a21f2d311b74cd2992fa3b
dav <73536483+Davizinhn@users.noreply.github.com>
2023-04-10 13:50:19 -0300

    fix

--- Assets/DiscordPresence.cs
+++ Assets/DiscordPresence.cs
@@ -10,6 +10,7 @@ public class DiscordPresence : MonoBehaviour
     Discord.ActivityManager activityManager;
     Discord.Activity activity;
     Discord.OverlayManager overlayManager;
+    System.Diagnostics.Process[] pname = System.Diagnostics.Process.GetProcessesByName("Discord");
 
     // Start is called before the first frame update
     void Awake()
@@ -18,35 +19,40 @@ public class DiscordPresence : MonoBehaviour
     }
     void Start()
     {
-        discord=new Discord.Discord(appid, (System.UInt64)Discord.CreateFlags.Default);
-        activityManager = discord.GetActivityManager();
-        activity = new Discord.Activity{
-            Details = "Loading",
-            State = "",
-            Assets = {
-                LargeImage = "logo"
-            }
-        };
-        activityManager.UpdateActivity(activity, (res) =>{
-            if(res==Discord.Result.Ok)
-            {
-                Debug.Log("Discord status set!");
-            }
-            else
-            {
-                Debug.LogError("Discord status failed!");
-            }
-        });
+        if (pname.Length != 0){
+            discord=new Discord.Discord(appid, (System.UInt64)Discord.CreateFlags.Default);
+            activityManager = discord.GetActivityManager();
+            activity = new Discord.Activity{
+                Details = "Loading",
+                State = "",
+                Assets = {
+                    LargeImage = "logo"
+                }
+            };
+            activityManager.UpdateActivity(activity, (res) =>{
+                if(res==Discord.Result.Ok)
+                {
+                    Debug.Log("Discord status set!");
+                }
+                else
+                {
+                    Debug.LogError("Discord status failed!");
+                }
+            });
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (pname.Length != 0){
         discord.RunCallbacks();
+        }
     }
 
     public void ChangePresence(string details, string state)
     {
+        if (pname.Length != 0){
         activity.Details = details;
         activity.State = state;
         activityManager.UpdateActivity(activity, (res) =>{
@@ -59,10 +65,13 @@ public class DiscordPresence : MonoBehaviour
                 Debug.LogError("Discord status failed!");
             }
         });
+        }
     }
 
     void OnApplicationQuit()
     {
+        if (pname.Length != 0){
         discord.Dispose();
+        }
     }
 }
