e36c3b5f76c22c8a0a9a5d355afbf2631b8b2f95
dav <73536483+Davizinhn@users.noreply.github.com>
2023-04-16 23:44:02 -0300

    sla

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -8,16 +8,11 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
     public Vector3 max;
-        public Vector3 min;
+    public Vector3 min;
 
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(Random.RandomRange(min.x, max.x), Random.RandomRange(min.y, max.y), Random.RandomRange(min.z, max.z)), Quaternion.identity);
-    }
-
-    void Update()
-    {
-
     }
 }
