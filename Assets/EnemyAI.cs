e73eb5b60e3d30c96cf3059ec9f3f4d38cd59f4a
dav <73536483+Davizinhn@users.noreply.github.com>
2023-07-13 02:52:34 -0300

    AI Nova pt2

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -23,6 +23,7 @@ public class EnemyAI : MonoBehaviour
     public float walkSpeed;
     public float runSpeed;
     GameObject[] randLocations;
+    Vector3 nil=new Vector3(0,0,0);
 
     public void ChangeToState(AIStates state)
     {
@@ -40,6 +41,9 @@ public class EnemyAI : MonoBehaviour
             case AIStates.Walking:
                 WalkingState();
                 break;
+            case AIStates.Chasing:
+                ChasingState();
+                break;
         }
     }
 
@@ -47,6 +51,7 @@ public class EnemyAI : MonoBehaviour
     public void IdleState()
     {
         agent.speed = 0;
+        VerifyForPlayer();
         if(!isLoopingIdle)
         {
             StartCoroutine(idleLoop());
@@ -56,25 +61,34 @@ public class EnemyAI : MonoBehaviour
     public IEnumerator idleLoop()
     {
         isLoopingIdle=true;
-        yield return new WaitForSeconds(Random.Range(2,5));
+        yield return new WaitForSeconds(Random.Range(3,5));
         if(curState == AIStates.Idle)
         {
             ChangeToState(AIStates.Walking);
         }
         isLoopingIdle=false;
-        StopCoroutine(idleLoop());
     }
 
     bool alreadyWalking = false;
     public void WalkingState()
     {
         agent.speed=walkSpeed;
-        if(!alreadyWalking)
+        VerifyForPlayer();
+        if(!alreadyWalking && ultimaLocation != nil)
+        {
+            alreadyWalking=true;
+            agent.SetDestination(ultimaLocation);
+        }
+        else if(!alreadyWalking && ultimaLocation==nil)
         {
             ChooseRandomLocation();
         }
         if(alreadyWalking && agent.remainingDistance==0f)
         {
+            if(agent.destination==ultimaLocation)
+            {
+                ultimaLocation=nil;
+            }
             ChangeToState(AIStates.Idle);
             alreadyWalking=false;
         }
@@ -83,9 +97,72 @@ public class EnemyAI : MonoBehaviour
     public void ChooseRandomLocation()
     {
         agent.SetDestination(randLocations[Random.Range(0, randLocations.Length-1)].transform.position);        
-        alreadyWalking=true;
         Debug.DrawLine(agent.transform.position, agent.destination, Color.blue, 10f);
+        alreadyWalking=true;
+    }
 
+    public void VerifyForPlayer()
+    {
+        int rayCount = 50;
+        float rayLength = 100f;
+        float angleBetweenRays = 2.5f; 
+        for (int i = 0; i < rayCount; i++)
+        {
+            Quaternion rotation = Quaternion.Euler(0f, i * angleBetweenRays - 40f, 0f);
+            Vector3 direction = rotation * transform.forward;
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, direction, out hit, rayLength))
+            {
+                Debug.DrawRay(transform.position, direction * hit.distance, Color.red);
+                if(hit.transform.gameObject.CompareTag("Player"))
+                {
+                    if(curState == AIStates.Chasing)
+                    {
+                        ultimaLocation = hit.transform.position;
+                        chasingPlayer = hit.transform;
+                    }
+                    else
+                    {
+                        ultimaLocation = hit.transform.position;
+                        chasingPlayer = hit.transform;
+                        ChangeToState(AIStates.Chasing);
+                    }
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, direction * rayLength, Color.green);
+            }
+        }
+    }
+
+    bool jaToPerdendo=false;
+    public void ChasingState()
+    {
+        agent.speed=runSpeed;
+        alreadyWalking=false;
+        VerifyForPlayer();
+        if(chasingPlayer!=null && !jaToPerdendo)
+        {
+            StartCoroutine(perderPlayer());
+        }
+        if(chasingPlayer==null)
+        {
+            ChangeToState(AIStates.Walking);
+        }
+        else
+        {
+            agent.SetDestination(chasingPlayer.transform.position);
+        }
+    }
+
+    public IEnumerator perderPlayer()
+    {
+        jaToPerdendo=true;
+        yield return new WaitForSeconds(2f);
+        chasingPlayer=null;
+        jaToPerdendo=false;
     }
 
 
