bb9ea852005614db65cf5da396c1c3435760c754
dav <73536483+Davizinhn@users.noreply.github.com>
2023-08-14 14:50:13 -0300

    0.9.2

--- Assets/NameFollow.cs
+++ Assets/NameFollow.cs
@@ -6,6 +6,7 @@ public class NameFollow : MonoBehaviour
 {
     public Transform camera1;
     public Transform camera2;
+    public Transform camera3;
 
     void Start()
     {
@@ -17,8 +18,15 @@ public class NameFollow : MonoBehaviour
     {
         if(camera1==null)
         {
-            camera2 = GameObject.FindWithTag("SpectatorCam").GetComponent<Camera>().transform;
-            transform.LookAt(transform.position + camera2.rotation * Vector3.forward, camera2.rotation * Vector3.up);
+            if(!camera3.gameObject.active)
+            {
+                camera2 = GameObject.FindWithTag("SpectatorCam").GetComponent<Camera>().transform;
+                transform.LookAt(transform.position + camera2.rotation * Vector3.forward, camera2.rotation * Vector3.up);
+            }
+            else if(camera3.gameObject.active)
+            {
+                transform.LookAt(transform.position + camera3.rotation * Vector3.forward, camera3.rotation * Vector3.up);
+            }
         }
         else
         {
